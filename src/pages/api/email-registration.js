69bf8f2c9384bfb1f8e14bd4be4220d20e907ccd
Jonathan Rodriguez <jonathan.rdzes@gmail.com>
2024-08-26 18:33:10 -0600

    Added missing folder

--- src/pages/api/email-registration.js
+++ src/pages/api/email-registration.js
@@ -4,7 +4,7 @@ import fs from "fs";
 const buildPath = () => {
   return process.env.ENVIRONMENT === "development"
     ? path.join(process.cwd(), "src", "data", "data.json")
-    : path.join(process.cwd(), "/tmp", "data.json");
+    : path.join(process.cwd(), "/tmp", "data", "data.json");
 };
 
 const extractData = (filePath) => {
